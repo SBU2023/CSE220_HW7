7babdd507fa58b5521d5d0ec03123078498bb00d
SBU2023 <140561879+SBU2023@users.noreply.github.com>
2023-11-23 16:56:19 -0500

    Update hw7b.asm

--- hw7b.asm
+++ hw7b.asm
@@ -1,1 +1,13 @@
+.data
+# The following you can use for pretty print the 
+# input and output as described in the document
+A:		.asciiz "A["
+B:		.asciiz "B["
+C:		.asciiz "]="
+spacechar:	.asciiz " "
+bar:		.asciiz "|"
 
+pinA: .space	400 # We will not change this
+pinB: .space	400 # We will not change this
+num: .word n  # We can change this 
+              # n x 4 <= 400 This is the constraint
